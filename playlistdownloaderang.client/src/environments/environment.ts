5a95e0ab5a9910406ab06e742e611302666a5766
Ben Small <llamsneb@gmail.com>
2024-03-10 20:55:24 -0500

    update exportApi url

--- playlistdownloaderang.client/src/environments/environment.ts
+++ playlistdownloaderang.client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   redirectUrl: 'https://green-bush-0657ada10.4.azurestaticapps.net',
-  exportApi: 'https://playlistdownloaderangserver.azurewebsites.net'
+  exportApi: 'https://playlistdownloaderangserver.azurewebsites.net/api/Export'
 };
