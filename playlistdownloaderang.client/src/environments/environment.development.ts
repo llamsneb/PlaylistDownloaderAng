bcca5dd1743518c5effca16f3c84baffb1bafc0f
Ben Small <llamsneb@gmail.com>
2024-03-10 20:42:39 -0500

    change exportApi to webapi url

--- playlistdownloaderang.client/src/environments/environment.development.ts
+++ playlistdownloaderang.client/src/environments/environment.development.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: false,
   redirectUrl: 'https://localhost:4200',
   exportApi: 'http://localhost:7235/api/Export'
-// webapi   'https://localhost:7030'
 };
